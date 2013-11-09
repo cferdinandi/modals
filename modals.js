bc4a757fbab104e5fe6a22d74e4786911097c509
Chris Ferdinandi <hello@gomakethings.com>
2013-11-09 11:22:24 -0500

    Converted from spaces to tabs
    http://lea.verou.me/2012/01/why-tabs-are-clearly-superior/

--- modals.js
+++ modals.js
@@ -1,151 +1,151 @@
 /* =============================================================
 
-    Modals v2.2
-    Simple modal dialogue pop-up windows by Chris Ferdinandi.
-    http://gomakethings.com
+	Modals v2.2
+	Simple modal dialogue pop-up windows by Chris Ferdinandi.
+	http://gomakethings.com
 
-    Free to use under the MIT License.
-    http://gomakethings.com/mit/
-    
+	Free to use under the MIT License.
+	http://gomakethings.com/mit/
+
  * ============================================================= */
 
 (function() {
 
-    'use strict';
+	'use strict';
 
-    // Feature test
-    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+	// Feature test
+	if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
 
-        // Function to show modal
-        var showModal = function (toggle) {
+		// Function to show modal
+		var showModal = function (toggle) {
 
-            // Define the modal
-            var dataID = toggle.getAttribute('data-target');
-            var dataTarget = document.querySelector(dataID);
+			// Define the modal
+			var dataID = toggle.getAttribute('data-target');
+			var dataTarget = document.querySelector(dataID);
 
-            // Define the modal background
-            var modalBg = document.createElement('div');
-            buoy.addClass(modalBg, 'modal-bg');
+			// Define the modal background
+			var modalBg = document.createElement('div');
+			buoy.addClass(modalBg, 'modal-bg');
 
-            // Activate the modal
-            buoy.addClass(dataTarget, 'active');
-            dataTarget.style.top = window.pageYOffset + 50 + 'px';
-            document.body.appendChild(modalBg);
+			// Activate the modal
+			buoy.addClass(dataTarget, 'active');
+			dataTarget.style.top = window.pageYOffset + 50 + 'px';
+			document.body.appendChild(modalBg);
 
-        };
+		};
 
-        // Function to hide all modals
-        var hideModals = function (modals, modalsBg) {
+		// Function to hide all modals
+		var hideModals = function (modals, modalsBg) {
 
-            // Hide all modals
-            [].forEach.call(modals, function (modal) {
-                buoy.removeClass(modal, 'active');
-            });
+			// Hide all modals
+			[].forEach.call(modals, function (modal) {
+				buoy.removeClass(modal, 'active');
+			});
 
-            // Hide all modal backgrounds
-            [].forEach.call(modalsBg, function (bg) {
-                document.body.removeChild(bg);
-            });
+			// Hide all modal backgrounds
+			[].forEach.call(modalsBg, function (bg) {
+				document.body.removeChild(bg);
+			});
 
-        };
-
-
-        // Define modals, modal toggle, and modal close
-        var modals = document.querySelectorAll('.modal');
-        var modalToggle = document.querySelectorAll('.modal-toggle');
-        var modalClose = document.querySelectorAll('.modal-close');
+		};
 
 
-        // When body is clicked
-        document.addEventListener('click', function() {
-        
-            // Hide all modals
-            hideModals(modals, document.querySelectorAll('.modal-bg'));
-            
-        }, false);
+		// Define modals, modal toggle, and modal close
+		var modals = document.querySelectorAll('.modal');
+		var modalToggle = document.querySelectorAll('.modal-toggle');
+		var modalClose = document.querySelectorAll('.modal-close');
 
 
-        // When body is tapped
-        document.addEventListener('touchstart', function() {
-        
-            // Hide all modals
-            hideModals(modals, document.querySelectorAll('.modal-bg'));
-            
-        }, false);
+		// When body is clicked
+		document.addEventListener('click', function() {
+
+			// Hide all modals
+			hideModals(modals, document.querySelectorAll('.modal-bg'));
+
+		}, false);
 
 
-        // For each modal toggle
-        [].forEach.call(modalToggle, function (toggle) {
+		// When body is tapped
+		document.addEventListener('touchstart', function() {
 
-            // When the modal toggle is clicked
-            toggle.addEventListener('click', function(e) {
+			// Hide all modals
+			hideModals(modals, document.querySelectorAll('.modal-bg'));
 
-                // Prevent the "close all modals" function
-                e.stopPropagation();
-
-                // Prevent the default link behavior
-                e.preventDefault();
-
-                // Show the modal
-                showModal(toggle);
-             
-            }, false);
-
-        });
+		}, false);
 
 
-        // For each modal close
-        [].forEach.call(modalClose, function (close) {
+		// For each modal toggle
+		[].forEach.call(modalToggle, function (toggle) {
 
-            // When the modal toggle is clicked
-            close.addEventListener('click', function(e) {
+			// When the modal toggle is clicked
+			toggle.addEventListener('click', function(e) {
 
-                // Prevent the default link behavior
-                e.preventDefault();
+				// Prevent the "close all modals" function
+				e.stopPropagation();
 
-                // Hide all modals
-                hideModals(modals, document.querySelectorAll('.modal-bg'));
+				// Prevent the default link behavior
+				e.preventDefault();
 
-            }, false);
+				// Show the modal
+				showModal(toggle);
 
-        });
+			}, false);
 
-
-        // For each modal window
-        [].forEach.call(modals, function (modal) {
-
-            // When the menu is clicked
-            modal.addEventListener('click', function(e) {
-
-                // Prevent the "close all dropdowns" function
-                e.stopPropagation();
-
-            }, false);
-
-            // When the menu is tapped
-            modal.addEventListener('touchstart', function(e) {
-
-                // Prevent the "close all dropdowns" function
-                e.stopPropagation();
-
-            }, false);
-            
-        });
+		});
 
 
-        // When key on keyboard is pressed
-        window.addEventListener('keydown', function (e) {
+		// For each modal close
+		[].forEach.call(modalClose, function (close) {
 
-            // If it's the esc key
-            if (e.keyCode == 27) {
-            
-                // Hide all modals
-                hideModals(modals, document.querySelectorAll('.modal-bg'));
-                
-            }
+			// When the modal toggle is clicked
+			close.addEventListener('click', function(e) {
 
-        }, false);
+				// Prevent the default link behavior
+				e.preventDefault();
 
-    }
+				// Hide all modals
+				hideModals(modals, document.querySelectorAll('.modal-bg'));
+
+			}, false);
+
+		});
+
+
+		// For each modal window
+		[].forEach.call(modals, function (modal) {
+
+			// When the menu is clicked
+			modal.addEventListener('click', function(e) {
+
+				// Prevent the "close all dropdowns" function
+				e.stopPropagation();
+
+			}, false);
+
+			// When the menu is tapped
+			modal.addEventListener('touchstart', function(e) {
+
+				// Prevent the "close all dropdowns" function
+				e.stopPropagation();
+
+			}, false);
+
+		});
+
+
+		// When key on keyboard is pressed
+		window.addEventListener('keydown', function (e) {
+
+			// If it's the esc key
+			if (e.keyCode == 27) {
+
+				// Hide all modals
+				hideModals(modals, document.querySelectorAll('.modal-bg'));
+
+			}
+
+		}, false);
+
+	}
 
 })();
